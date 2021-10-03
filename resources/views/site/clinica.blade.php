4f23d701c1554f0f285762752be3c5aeeefba247
evvvrado <everaldocrj@gmail.com>
2021-10-03 11:46:06 -0300

    Banner

--- resources/views/site/clinica.blade.php
+++ resources/views/site/clinica.blade.php
@@ -45,21 +45,6 @@
     
     <section class="container-fluid s_destaque">
       <div class="container-fav">
-        <div class="_content fade">
-          <div class="_line">
-            <button>
-              <img src="{{ asset('site/img/arrowright.svg') }}" alt="Seta para direita" />
-            </button>
-          </div>
-          <h6></h6>
-          <div class="_svg">
-            <img src="{{ asset('site/img/svg_poweroflife_white.svg') }}" alt="Power of Life" />
-          </div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-          </p>
-        </div>
       </div>
     </section>
 
